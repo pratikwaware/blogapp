e92e73adb0a29da677a5bdd5a407cf89f723d9f9
pratikwaware <pratikwaware919@gmail.com>
2020-07-31 21:00:31 +0530

    Updates

---
+++ precache-manifest.35396022a096883266060ae9115454e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a084e08f0a6ea899b710edb76fe589a4",
+    "url": "/blogapp/index.html"
+  },
+  {
+    "revision": "f917761d5dec6eed99ea",
+    "url": "/blogapp/static/css/main.8d61b4e9.chunk.css"
+  },
+  {
+    "revision": "69e8bb43159ce2e29a0a",
+    "url": "/blogapp/static/js/2.78185a2a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/blogapp/static/js/2.78185a2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f917761d5dec6eed99ea",
+    "url": "/blogapp/static/js/main.791216d6.chunk.js"
+  },
+  {
+    "revision": "8c6cdb8667c097733472",
+    "url": "/blogapp/static/js/runtime-main.95fe9976.js"
+  }
+]);
